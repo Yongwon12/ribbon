963c89d759202871adc2b029af651dfd949b0b64
gim-yong-won <tjaaj159@naver.com>
2022-12-10 00:00:14 +0900

    php_mysql 각 db table에 데이터저장

--- .idea/server/apis/boardwritedbput.php
+++ .idea/server/apis/boardwritedbput.php
@@ -3,13 +3,13 @@
 include_once('../common/header.php');
 include_once('../common/response.php');
 include_once('../common/encipher.php');
-$conn = mysqli_connect("172.30.1.24", "yongwon2", "Dyddnjs3401!", 'ribbonProject');
+$conn = mysqli_connect("192.168.219.104", "yongwon2", "Dyddnjs3401!", 'ribbonProject');
 
 //method file_get_contents() get all data send via API call.
 //json_decode() decodes data as json and assign to variable $user.
 $user = json_encode(file_get_contents("php://input"));
 //validation whether user data is having name or not. similarly email, password etc.
-$sql = "SELECT * FROM user";
+$sql = "SELECT * FROM boardwrite";
 
 $result = mysqli_query($conn,$sql);
 $data = array();
@@ -18,19 +18,17 @@ if ($result)
     while ($row = mysqli_fetch_array($result))
     {
         // or select*from 테이블 where id = 3 and userid = 3 이런식으로 불러오기 + 배열형식
-        array_push($data, array('id' => $row[0], 'username' => $row[1],
-            'password'=>$row[2],'email'=>$row[3],'nickname'=>$row[4],'mobile'=>$row[5],
-            'create_date'=>$row[6], 'modify_date'=>$row[7],'birth'=>$row[8],
-            'image'=>$row[9],'gender'=>$row[10]));
+        array_push($data, array('id' => $row[0], 'userid' => $row[1],
+            'title'=>$row[2],'description'=>$row[3],'img'=>$row[4],'writedate'=>$row[5]));
 
     }
-    $json = json_encode(array("user" => $data), JSON_PRETTY_PRINT + JSON_UNESCAPED_UNICODE);
-        echo $json;
-    }
+    $json = json_encode(array("boardwrite" => $data), JSON_PRETTY_PRINT + JSON_UNESCAPED_UNICODE);
+    echo $json;
+}
 else
-    {
+{
     echo "sql 처리중 에러";
     echo mysqli_error($conn);
-    }
+}
 mysqli_close($conn);
 ?>
